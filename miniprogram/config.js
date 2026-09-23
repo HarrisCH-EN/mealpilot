17ccf14a187ae813ee0e88845763600d88abe8fe
chenhqi <chenhqi@outlook.com>
2026-09-23 19:57:11 +0800

    准备部署

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -9,7 +9,7 @@ const environments = {
   }
 }
 
-const activeEnvironment = 'development'
+const activeEnvironment = 'production'
 
 function validateEnvironmentConfig(environment, config) {
   if (!config) throw new Error(`未找到环境配置：${environment}`)
